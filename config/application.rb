83a9b79c65b9dc286eda69f4fb818da19fe1423c
Marty McGee <mcgee.marty@gmail.com>
2025-12-26 10:12:42 -0800

    [MM] 1.1.1-beta.1: ThreeD.Bot: mime types

--- config/application.rb
+++ config/application.rb
@@ -93,6 +93,7 @@ module FarmBot
         "#{ENV.fetch("API_HOST")}:#{API_PORT}",
         "#{ENV.fetch("API_HOST")}:3808",
         "blob:", # 3D
+        "threed.bot", # MM
       ]
       config.csp = {
         default_src: %w(https: 'self'),
