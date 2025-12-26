582132d3076a5bc15603028b4d506ad6df6c6bf0
Marty McGee <mcgee.marty@gmail.com>
2025-12-26 09:09:45 -0800

    [MM] 1.1.1-beta.1: ThreeD.Bot: mime types

---
+++ config/initializers/mime_types.rb
@@ -0,0 +1,7 @@
+# config/initializers/mime_types.rb
+Mime::Type.register "model/gltf-binary", :glb
+Mime::Type.register "model/gltf+json", :gltf
+Mime::Type.register "application/octet-stream", :fbx
+Mime::Type.register "application/octet-stream", :obj
+Mime::Type.register "application/octet-stream", :mtl
+Mime::Type.register "application/octet-stream", :stl
